45cecd4caff819eb1448cfb91d4781c7ed4abda9
Rokki Khazratov <rokkikhazratov@gmail.com>
2024-10-06 19:39:17 +0500

    feedback form style

--- api/forms.py
+++ api/forms.py
@@ -5,3 +5,8 @@ class FeedbackForm(forms.ModelForm):
     class Meta:
         model = Feedback
         fields = ['member_id', 'feedback_body']
+        
+        labels = {
+            'member_id': 'ID Участника',  
+            'feedback_body': 'Текст отзыва'  
+        }
