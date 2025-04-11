9c817e7b5242e3260fca91984837ccc1500238cf
Rokki Khazratov <rokkikhazratov@gmail.com>
2025-04-11 06:40:26 +0500

    ready

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.router')),  # Подключаем URL-ы из api.router
+    path('', include('api.router')),  # Подключаем URL-ы из api.router
 ]
 
 # Добавляем обработку медиа-файлов в режиме разработки
